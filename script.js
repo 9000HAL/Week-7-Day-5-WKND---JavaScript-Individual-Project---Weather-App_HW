28a48eebf1c39840c4380c3a3a390fb0edaf85a5
9000HAL <gsanchezscs@gmail.com>
2023-08-06 22:36:12 -0700

    .js wrapping ///// check all

--- script.js
+++ script.js
@@ -8,42 +8,27 @@ function fetchWeather() {
 
     // the API URl with our API key and city name
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a9042d63ba4ea392d44f3d4175a0d8d0`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a9042d63ba4ea392d44f3d4175a0d8d0`;
 
 // using fetch to get data from the API
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
         // display the city name
-        document.getElementById('cityName').innerText = data.name;
+            document.getElementById('cityName').innerText = data.name;
 
         // convert temperature from kelvin to fahrenheit
-        const highTemp = ((data.main.temp_max - 273.15) * 9/5 +32).toFixed(2);
-        const lowTemp = ((data.main.temp_min - 273.15) * 9/5 +32).toFixed(2);
+            const highTemp = ((data.main.temp_max - 273.15) * 9/5 +32).toFixed(2);
+            const lowTemp = ((data.main.temp_min - 273.15) * 9/5 +32).toFixed(2);
 
         //display the weather data
-        document.querySelector('#high span').innerText = highTemp + "°F";
-        document.querySelector('#low span').innerText = lowTemp + "°F";
-        document.querySelector('#forecast span').innerText = data.weather[0].description;
-        document.querySelector('#humidity span').innerText = data.main.humidity
-
-
-
-
-
-
-
-
-
-
-
-
-
-    })
-
-
-
-
-
-
+            document.querySelector('#high span').innerText = highTemp + "°F";
+            document.querySelector('#low span').innerText = lowTemp + "°F";
+            document.querySelector('#forecast span').innerText = data.weather[0].description;
+            document.querySelector('#humidity span').innerText = data.main.humidity + "%";
+        })
+    //basic error handling for unrecognized cities, error inputs, etc
+        .catch(error => {
+            alert("An error occured, please try again entering a CITY NAME only please, thanks for your time!")
+        });
 }
